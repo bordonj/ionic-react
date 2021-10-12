ee50e5803522f890170832e67e9e721cea01ef54
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-10-12 10:30:52 -0700

    add code to render when data received

--- src/components/GetPlanets.js
+++ src/components/GetPlanets.js
@@ -1,52 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import { useQuery, gql } from '@apollo/client'
-import { LOAD_PLANETS } from '../GraphQL/Queries'
+import { useEffect, useState } from 'react';
+import { useQuery } from '@apollo/client';
+import { IonPage, IonSearchbar, IonContent, IonList, IonItem } from "@ionic/react";
+import { LOAD_PLANETS } from '../GraphQL/Queries';
 
 function GetPlanets() {
   const [searchTerm, setSearchTerm] = useState('');
   const [results, setResults] = useState([]);
   
-  const {error, loading, data} = useQuery(LOAD_PLANETS);
+  const { data } = useQuery(LOAD_PLANETS);
   
-  const response = useQuery(LOAD_PLANETS);
 
   useEffect(() => {
-    const search = async () => {
-
+    if (data) {
+      setResults(data.allPlanets.planets)
     }
-    console.log(data)
-  }, [data])
+  }, [data]);
 
   return (
-    <div className="get-planets">
-      <input
-        type="text"
-        placeholder="Search..."
-        onChange={event => setSearchTerm(event.target.value)}
-      />
-      {data.allPlanets.planets.filter(val => {
-        if (searchTerm == "") {
-          return val
-        } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
-          return val
-        }
-      }).map((val, key) => {
-        return (
-          <div className="planet" key={key}>
-            <h3>{val.name}</h3>
-          </div>
-        )
-      })}
-      {/* <div>
-        {data.allPlanets.planets.map(planet => {
-          return (
-            <div key={planet.id}>
-              <h1>{planet.name}</h1>
-            </div>
-          )
-        })}
-      </div> */}
-    </div>
+    <IonPage>
+      <div className="get-planets">
+        <IonSearchbar
+          onIonChange={event => setSearchTerm(event.target.value)}
+          placeholder="Search..."
+        />
+        <IonContent
+          style={{height: "100vh"}}>
+          <IonList>
+          {results.filter(val => {
+            if (searchTerm == "") {
+              return val;
+            } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+              return val;
+            }
+          }).map((val, key) => {
+            return (
+              <IonItem>
+                <div className="planet" key={key}>
+                  <h3>{val.name}</h3>
+                </div>
+              </IonItem>
+            )
+          })}
+          </IonList>
+        </IonContent>
+      </div>
+    </IonPage>
   )
 }
 
