0be41859a4eb5979232e82ef4e951653da62fe2d
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-10-13 12:24:19 -0700

    add planet details page

---
+++ src/components/PlanetDetails.js
@@ -0,0 +1,65 @@
+import { useState, useEffect } from 'react';
+import { IonPage, IonContent, IonList, IonItem, IonHeader, IonToolbar, IonTitle, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCard, IonCardContent, IonButton } from "@ionic/react";
+
+const PlanetDetails = ({planet, planetVisible, setPlanetVisible}) => {
+  const [thisPlanet, setThisPlanet] = useState(planet);
+
+  let orbitalPeriod = planet.orbitalPeriod? planet.orbitalPeriod: "n/a";
+  let population = planet.population? planet.population: "n/a";
+
+  useEffect(() => {
+    if (planet) {
+      setThisPlanet(planet);
+    }
+    
+  }, [thisPlanet]);
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>
+            {planet.name} Details
+          </IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardSubtitle>
+              Here you will find some details about the planet
+            </IonCardSubtitle>
+            <IonCardTitle>
+              Name: {planet.name}
+            </IonCardTitle>
+
+            <IonCardContent>
+              <IonList>
+                <IonItem>Orbital Period: {orbitalPeriod}</IonItem>
+                <IonItem>Population: {population}</IonItem>
+                <IonItem>Terrain: {planet.terrains.map((terrain, idx) => {
+                  return (
+                    <div key={idx}>
+                      <IonItem>{terrain}</IonItem>
+                    </div>
+                  )
+                })}</IonItem>
+                <IonItem>Climates: {planet.climates.map((climate, idx) => {
+                  return (
+                    <div key={climate}>
+                      <IonItem>{climate}</IonItem>
+                    </div>
+                  )
+                })}</IonItem>
+              </IonList>
+            </IonCardContent>
+          </IonCardHeader>
+        </IonCard>
+      <IonButton onClick={()=> setPlanetVisible(!planetVisible)}>Back</IonButton>
+      </IonContent>
+    </IonPage>
+  )
+  
+}
+
+export default PlanetDetails
