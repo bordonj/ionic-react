e608a6060f2bee669e3fcdffb9a11ae593840edb
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-10-13 14:50:05 -0700

    add pointer to planet names

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import './index.css';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App.js';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
