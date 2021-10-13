338b9222fd717656e4a8ef5fb20b3435c291c55e
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-10-13 12:24:00 -0700

    set planet when name of planet clicked

--- src/components/GetPlanets.js
+++ src/components/GetPlanets.js
@@ -1,58 +1,87 @@
 import { useEffect, useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { IonPage, IonSearchbar, IonContent, IonList, IonItem } from "@ionic/react";
-import { LOAD_PLANETS } from '../GraphQL/Queries';
-import { planet } from 'ionicons/icons';
+import { LOAD_PLANETS, LOAD_PLANET } from '../GraphQL/Queries';
+import PlanetDetails from './PlanetDetails';
 
 function GetPlanets() {
   const [searchTerm, setSearchTerm] = useState('');
   const [results, setResults] = useState([]);
-  
+  const [id, setId] = useState('');
+  const [planet, setPlanet] = useState({});
+  const [planetVisible, setPlanetVisible] = useState(false);
+
   const { data } = useQuery(LOAD_PLANETS);
-  
+  const { data: datum } = useQuery(LOAD_PLANET, {
+    variables: { "planetId": id }
+  });
+
+  const handleChangingPlanetId = id => {
+    setId(id);
+    console.log(id);
+    setPlanetVisible(true);
+  }
 
   useEffect(() => {
     if (data) {
       setResults(data.allPlanets.planets);
     }
-  }, [data]);
+    console.log('datum', datum)
+    if (datum) {
+      setPlanet(datum.planet)
+    }
+  }, [data, datum]);
 
   const sorted = [...results];
   sorted.sort((a, b) => {
-    return (a.name < b.name) ? -1 : (a > b) ? 1 : 0;
+    return (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0;
   });
-  console.log(sorted)
 
-  return (
-    <IonPage>
-      <div className="get-planets">
-        <IonSearchbar
-          onIonChange={event => setSearchTerm(event.target.value)}
-          placeholder="Search..."
-        />
-        <IonContent
-          style={{height: "100vh"}}>
-          <IonList>
-          {sorted.sort().filter(val => {
-            if (searchTerm == "") {
-              return val;
-            } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
-              return val;
-            }
-          }).map((val, key) => {
-            return (
-              <IonItem key={key}>
-                <div className="planet" key={planet.id}>
-                  <h3>{val.name}</h3>
-                </div>
-              </IonItem>
-            )
-          })}
-          </IonList>
-        </IonContent>
-      </div>
-    </IonPage>
-  )
+  if (!planetVisible) {
+    return (
+      <IonPage>
+        <div className="get-planets">
+          <IonSearchbar
+            onIonChange={event => setSearchTerm(event.target.value)}
+            placeholder="Search..."
+          />
+          <IonContent
+            style={{height: "100vh"}}>
+            <IonList>
+            {sorted.sort().filter(val => {
+              if (searchTerm == "") {
+                return val;
+              } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                return val;
+              }
+            }).map((val, key) => {
+              return (
+                <IonItem key={key}>
+                  <div 
+                    className="planet" 
+                    key={val.id}
+                    onClick={() => handleChangingPlanetId(val.id)}
+                  >
+                    <h3>{val.name}</h3>
+                  </div>
+                </IonItem>
+              )
+            })}
+            </IonList>
+          </IonContent>
+        </div>
+      </IonPage>
+    )
+  } else {
+    return (
+      <PlanetDetails 
+        planet={planet}
+        planetVisible={planetVisible}
+        setPlanetVisible={setPlanetVisible}
+      />
+    )
+  }
+
 }
 
 export default GetPlanets;
