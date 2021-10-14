fd3b5a20634369ba4bd4ca5c329413158f0c4179
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-10-13 17:18:19 -0700

    add code to show data in arrays

--- src/components/PlanetDetails.js
+++ src/components/PlanetDetails.js
@@ -1,28 +1,67 @@
 import { useState, useEffect } from 'react';
 import { IonPage, IonContent, IonList, IonItem, IonHeader, IonToolbar, IonTitle, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCard, IonCardContent, IonButton } from "@ionic/react";
 
-const PlanetDetails = ({planet, planetVisible, setPlanetVisible}) => {
-  const [thisPlanet, setThisPlanet] = useState(planet);
-  const [terrains, setTerrains] = useState(planet.terrains);
-  const [climates, setClimates] = useState(planet.climates)
+const PlanetDetails = (props) => {
+  const [thisPlanet, setThisPlanet] = useState(props.planet);
+  const [terrains, setTerrains] = useState(props.planet.terrains);
+  const [climates, setClimates] = useState(props.planet.climates)
+  console.log('this planet', thisPlanet)
+  console.log('props', props)
+  console.log('props planet', props.planet)
+  console.log('props planet terrains', props.planet.terrains)
 
-  let orbitalPeriod = planet.orbitalPeriod? planet.orbitalPeriod: "n/a";
-  let population = planet.population? planet.population: "n/a";
+  let orbitalPeriod = props.planet.orbitalPeriod ? props.planet.orbitalPeriod: "n/a";
+  let population = props.planet.population ? props.planet.population: "n/a";
 
   useEffect(() => {
-    if (planet) {
-      setThisPlanet(planet);
-      console.log('terrains', terrains)
-      setTerrains(planet.terrains);
-    }
-  }, [thisPlanet]);
+    // if (thisPlanet) {
+    //   setThisPlanet(planet);
+    //   setTerrains(planet.terrains);
+    // }
+    setThisPlanet(props.planet);
+    setTerrains(props.planet.terrains);
+    
+  }, [thisPlanet, terrains]);
 
+  const showTerrain = () => {
+    if (props?.planet?.terrains) {
+      return (
+        <>
+          <h2>Terrain:</h2> {props.planet?.terrains?.map((terrain, idx) => {
+            return (
+              <IonItem key={idx}> 
+                {terrain}
+                </IonItem>
+            )
+          })}
+        </>
+      )
+    }
+  };
+
+  const showClimates = () => {
+    if (props?.planet?.climates) {
+      return (
+        <>
+          <h2>Climates:</h2> {props.planet.climates.map((climate) => {
+            return (
+              <div key={climate}>
+                <IonItem>{climate}</IonItem>
+              </div>
+            )
+          })}
+        </>
+      )
+    }
+  }
+  
+  console.log('terrains', terrains)
     return (
       <IonPage>
         <IonHeader>
           <IonToolbar>
             <IonTitle>
-              {planet.name} Details
+              {props.planet.name} Details
             </IonTitle>
           </IonToolbar>
         </IonHeader>
@@ -33,29 +72,21 @@ const PlanetDetails = ({planet, planetVisible, setPlanetVisible}) => {
                 Here you will find some details about the planet
               </IonCardSubtitle>
               <IonCardTitle>
-                Name: {planet.name}
+                Name: {props.planet.name}
               </IonCardTitle>
               <IonCardContent>
                 <IonList>
-                  <IonItem>Orbital Period: {orbitalPeriod}</IonItem>
-                  <IonItem>Population: {population}</IonItem>
-                  {/* <IonItem>Terrain: {planet.terrains.map((terrain, idx) => {
-                    return (
-                      <div key={idx}>
-                        <IonItem>{terrain}</IonItem>
-                      </div>
-                    )
-                  })}</IonItem>
-                  <IonItem>Climates: {planet.climates.map((climate, idx) => {
-                    return (
-                      <div key={climate}>
-                        <IonItem>{climate}</IonItem>
-                      </div>
-                    )
-                  })}</IonItem> */}
+                  <IonItem>
+                    Orbital Period: {orbitalPeriod}
+                  </IonItem>
+                  <IonItem>
+                    Population: {population}
+                  </IonItem>
+                  {showTerrain()}
+                  {showClimates()}
                 </IonList>
               </IonCardContent>
-              <IonButton onClick={()=> setPlanetVisible(!planetVisible)}>Back</IonButton>
+              <IonButton onClick={()=> props.setPlanetVisible(!props.planetVisible)}>Back</IonButton>
             </IonCardHeader>
           </IonCard>
         </IonContent>
